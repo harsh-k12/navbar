29810217912aaa0e29a878196aaecb137a0a1d2a
hrsh9 <harshsinha.1203@gmail.com>
2018-08-08 18:22:59 -0700

    navbar

---
+++ j.js
@@ -0,0 +1,48 @@
+//color effect when curser is in it
+$(document).ready(() =>{
+	
+	
+	$(".first").mouseover(() =>{
+		$(".first").css("background","orange")
+		$(".first").css("border-radius","20px")
+		$(".first").css("font-style","italic")
+	})
+	$(".first").mouseout(() =>{
+		$(".first").css("background","khaki")
+		$(".first").css("font-style","normal")
+	})
+	
+	
+			$(".second").mouseover(() =>{
+				$(".second").css("background","orange")
+				$(".second").css("border-radius","20px")
+				$(".second").css("font-style","italic")
+			})
+			$(".second").mouseout(() =>{
+				$(".second").css("background","khaki")
+				$(".second").css("font-style","normal")
+			})
+	
+		
+					$(".third").mouseover(() =>{
+						$(".third").css("background","orange")
+						$(".third").css("border-radius","20px")
+						$(".third").css("font-style","italic")
+					})
+					$(".third").mouseout(() =>{
+						$(".third").css("background","khaki")
+						$(".third").css("font-style","normal")
+					})
+	
+
+							$(".fourth").mouseover(() =>{
+								$(".fourth").css("background","orange")
+								$(".fourth").css("border-radius","20px")
+								$(".fourth").css("font-style","italic")
+							})
+							$(".fourth").mouseout(() =>{
+								$(".fourth").css("background","khaki")
+								$(".fourth").css("font-style","normal")
+							})
+	
+})
